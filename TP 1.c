a6202588d06d318d94403a0b0124bde6b2d342e3
BENMEZIAN <44295887+BENMEZIAN@users.noreply.github.com>
2019-06-09 13:31:00 +0100

    Add files via upload

---
+++ TP 1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float Newton(float ,float );
+
+main() {
+	     
+	     int tab[10],n,i;  float R,precision=0.001;
+	     
+	         printf("donner la valeur de n:");
+	         do{
+	         	scanf("%d",&n);
+			 }while(n<=0 ||  n>10);
+			 
+			 for(i=0;i<n;i++){
+			 	printf("l'element tab[%d] est:",i);
+			 	scanf("%d",&tab[i]);
+			 }
+			 
+			 printf("\n");
+			 
+			  for(i=0;i<n;i++){
+			 	printf("l'element tab[%d] est:%d\n",i,tab[i]);
+			 }
+			 
+			  printf("\n");
+			 
+			  for(i=0;i<n;i++){
+			 R=Newton(tab[i],precision);
+			 printf("la racine de %d vaut:%f\n",tab[i],R); 
+			 }
+			 
+			 
+	return 0;
+}
+
+
+float Newton(float a,float eps){
+	
+	float u,v,d;
+	
+	u=a/2;
+	v=0.5*(u+a/u);
+	d=fabs(u-v);
+	while(d>=eps){
+		u=v;
+		v=0.5*(u+a/u);
+		d=fabs(u-v);
+	}
+	return v;
+}
